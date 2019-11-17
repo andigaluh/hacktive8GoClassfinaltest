ce39fccb2191c06635de74028dfe61ea00b43ce5
andigaluh <andi@komunigrafik.com>
2019-11-17 08:41:49 +0700

    initialize

--- Hacktive8GoClassweb/application/views/partials/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div class="col-md-2">
-            <ul>
-                <li><a href="<?php echo site_url('dashboard')?>">Dashboard</a></li>
-                <li><a href="<?php echo site_url('adminPosts')?>">Posts</a></li>
-            </ul>
-        </div>
